03a0a151eaf70734700d8e233dfbe9e2528a9510
bnyte <bnytezz@gmail.com>
2022-10-31 00:48:57 +0800

    project init

---
+++ koudi-product/src/main/java/org/kushinae/koudi/product/ProductMain.java
@@ -0,0 +1,16 @@
+package org.kushinae.koudi.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 商品服务启动程序
+ * @author bnyte
+ * @since 1.0.0
+ */
+@SpringBootApplication
+public class ProductMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ProductMain.class, args);
+    }
+}
