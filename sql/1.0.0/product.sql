1914cd7873ee17d0f095596f855e0bbcbd417f2a
bnyte <bnytezz@gmail.com>
2022-11-17 13:39:46 +0800

    docs: update koudi-product init sql

--- sql/1.0.0/product.sql
+++ sql/1.0.0/product.sql
@@ -1,4 +1,8 @@
-create table `t_product_category`
+create database koudi_product;
+
+use koudi_product;
+
+create table if not exists t_product_category
 (
     `id`                  bigint NOT NULL AUTO_INCREMENT comment '分类id',
     `name`                varchar(50)  default null comment '分类名称',
@@ -19,7 +23,7 @@ create table `t_product_category`
     PRIMARY KEY (`id`)
 ) comment='商品三级分类';
 
-create table t_product_brand
+create table if not exists  t_product_brand
 (
     id                    bigint not null primary key auto_increment comment '品牌id',
     name                  char(50) comment '品牌名',
@@ -37,7 +41,7 @@ create table t_product_brand
     `deleted`             tinyint(1) DEFAULT '0' comment '是否逻辑删除 0否 1是 默认否'
 ) comment '品牌表';
 
-create table t_product_attr_group
+create table if not exists  t_product_attr_group
 (
     id                    bigint primary key not null auto_increment comment '分组id',
     attr_group_name       char(20) comment '组名',
@@ -54,7 +58,7 @@ create table t_product_attr_group
     `deleted`             tinyint(1) DEFAULT '0' comment '是否逻辑删除 0否 1是 默认否'
 ) comment '属性分组表';
 
-create table t_category_brand_relation
+create table if not exists  t_category_brand_relation
 (
     id                  bigint(20) not null auto_increment primary key,
     brand_id            bigint(20) default null comment '品牌id',
